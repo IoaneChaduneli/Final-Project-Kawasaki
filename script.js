5593a053c78d08fe3b1e7c2d9b77d0e4c8b89d8e
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2022-08-25 14:44:45 +0400

    header and toggle-bar

---
+++ script.js
@@ -0,0 +1,8 @@
+let toogleButton=document.getElementById('burger');
+let navigation=document.getElementById('nav');
+
+toogleButton.addEventListener('click', function() {
+navigation.classList.toggle('active');
+}) 
+
+
