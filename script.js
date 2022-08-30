0c688172c3dbe8f206e273c7b2ba2f7be84a44ba
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2022-08-30 22:29:35 +0400

    changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+// burger-navigation
 let toogleButton=document.getElementById('burger');
 let navigation=document.getElementById('nav');
 
 toogleButton.addEventListener('click', function() {
 navigation.classList.toggle('active');
-}) 
-
+});
 
